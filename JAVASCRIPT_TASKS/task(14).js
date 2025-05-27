e14f9cb942d0c22dfadce6dc6a1e04682ae3bdad
Cherala Harshini <cheralaharshini@gmail.com>
2025-05-27 17:58:12 +0530

    Add files via upload

---
+++ JAVASCRIPT_TASKS/task(14).js
@@ -0,0 +1,16 @@
+
+// jQuery code
+$(document).ready(function () {
+  // Handle click event on register button
+  $('#registerBtn').click(function () {
+    console.log("Register button clicked");
+    
+    // Show event cards with fadeIn effect
+    $('.eventCard').fadeIn(500);
+
+    // Example: Hide event cards after 3 seconds with fadeOut
+    setTimeout(() => {
+      $('.eventCard').fadeOut(500);
+    }, 3000);
+  });
+});
